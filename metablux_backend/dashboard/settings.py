7df6f9685044dc31b90813b5803355e262321ede
MARTIN <martin@lesoko.in>
2026-02-08 14:23:36 +0530

    django + react ts created

--- metablux_backend/dashboard/settings.py
+++ metablux_backend/dashboard/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open(BASE_DIR.parent / "metablux_testing.json") as f:
+with open(BASE_DIR.parent / "../metablux_testing.json") as f:
     get_config = json.load(f)
 
 # Quick-start development settings - unsuitable for production
